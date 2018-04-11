99030a8c674ddd806a1f78286aa52d36dd507375
ReinA <xluo1@stuy.edu>
2018-04-11 19:17:10 -0400

    created files for HW 95 and started working on maze

---
+++ .#MazeSolver.java
@@ -0,0 +1,1 @@
+reinaaaa@ReinAs-iMac.local.999
---
+++ MazeSolver.java
@@ -0,0 +1,10 @@
+/**
+  Test Maze class.
+
+  Requires command line arguments:
+  o  the name of a file containing a maze.
+  o  the rank and file where an explorer is starting
+
+  For example,
+      java UserOfMaze mazes/4cell_treasureWest.txt -1 -1  # no explorer
+ */
