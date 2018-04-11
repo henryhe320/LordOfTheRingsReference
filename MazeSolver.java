1927f6f5e03d3532461497749c5bd5adc34179c6
xluo1234 <34110670+xluo1234@users.noreply.github.com>
2018-04-11 19:36:15 -0400

    Pseudocode for MazeSolver

--- MazeSolver.java
+++ MazeSolver.java
@@ -1,10 +1,26 @@
 /**
-  Test Maze class.
+  Returns a boolean for this statement: Returns the Boolean value of the statement “the maze is navigable, having a legal travel path that is continuous from explorer's current position to treasure”
 
-  Requires command line arguments:
-  o  the name of a file containing a maze.
-  o  the rank and file where an explorer is starting
 
-  For example,
-      java UserOfMaze mazes/4cell_treasureWest.txt -1 -1  # no explorer
+ 
  */
+
+public class MazeSolver{
+    Boolean navigable;
+    Maze inProgress;
+    Maze snapShot;
+
+    private Boolean solver(){
+	if (inProgress.explorerIsOnA == inProgress.TREASURE)
+	    navigable = true;
+	else{
+	    for(int i = 1; i < 9; i * 2){
+		snapShot = new Maze(inProgress);
+		inProgress.go(i);
+		if(inProgress.exlorerIsOnA() == inProgress.STEPPING_STONE){
+		}
+		inProgress = snapShot;
+	    }
+		 
+    }
+}
